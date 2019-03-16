8706217280652207ffe60b809b1271a807b824d3
Kamalakannan <kamalakkannan_rajamanickam@trimble.com>
2019-03-16 11:44:48 +0530

    Created a baseclass

---
+++ src/com/git/tests/Baseclass.java
@@ -0,0 +1,11 @@
+package com.git.tests;
+
+public class Baseclass {
+
+	public static void main(String[] args) {
+		System.out.println("Created a BASECLASS");
+		System.out.println("DAY 1 class");
+
+	}
+
+}
