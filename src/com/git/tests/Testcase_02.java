be7bbffad925873c6c4f94de5a10fa4c909c6bcd
mano3590 <jjmano2008@gmail.com>
2019-03-18 23:06:25 +0530

    Added a frame

--- src/com/git/tests/Testcase_02.java
+++ src/com/git/tests/Testcase_02.java
@@ -1,10 +1,24 @@
 package com.git.tests;
 
-public class Testcase_02 {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+public class Testcase_02 
+{
 
+	public static void main(String[] args) 
+	{
+
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\RAJ\\eclipse-workspace\\OrangePage\\driver\\chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		driver.get("http://demo.guru99.com/test/guru99home/");
+		
+		driver.switchTo().frame("a077aa5e");
+		WebElement txtUser = driver.findElement(By.xpath("//img[@src='Jmeter720.png']"));
+		txtUser.click();
+		
 	}
 
 }
