dc22930fe587e941557d903ed06ce86c5afd5f56
psyan10041992 <psyan007@gmail.com>
2019-03-20 00:51:55 +0530

    drag and drop program updated

--- src/com/git/tests/TestCase_04.java
+++ src/com/git/tests/TestCase_04.java
@@ -1,10 +1,28 @@
 package com.git.tests;
 
+import java.awt.Desktop.Action;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
 public class TestCase_04 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		//* created by prasanth*\\
+		
+	System.setProperty("webdriver.chrome.driver", "C:\\Users\\prasanth\\Desktop\\psyan\\GIT10AM_WEEKEND\\driver\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("http://demo.guru99.com/test/drag_drop.html");
+		WebElement src = driver.findElement(By.xpath("(//a[@class='button button-orange'])[5]"));
+		
+		WebElement desc = driver.findElement(By.xpath("(//li[@class='placeholder'])[1]"));
+		
+		Actions ac = new Actions(driver);
+		ac.dragAndDrop(src, desc).perform();
+		
 	}
 
 }
